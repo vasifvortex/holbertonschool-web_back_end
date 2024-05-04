15a1d5455063c93fce4dd88ebf1df25c3934ae50
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-05-05 00:20:28 +0400

    Update 4-tasks.py

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -8,8 +8,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Wait for a random delay between 0 and max_delay"""
-    delays = []
-    for _ in range(n):
-        delays.append(await wait_random(max_delay))
-    delays.sort()
-    return delays
+    delays = [wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
