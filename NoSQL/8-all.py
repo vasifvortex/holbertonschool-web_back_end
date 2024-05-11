94825550d4046f7f628ddebc6d3e520b5db3e763
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-05-11 21:50:39 +0400

    Update 8-all.py

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-"""Listing all documents in a collection"""
+""" List all documents in Python """
+import pymongo
 
 
 def list_all(mongo_collection):
-    """Listing all documents in a collection"""
-    all = mongo_collection.find()
-    
-    if (len(list(all)) > 0):
-        return all
-    return []
+    """ lists all documents in a collection """
+    if not mongo_collection:
+        return []
+    return list(mongo_collection.find())
